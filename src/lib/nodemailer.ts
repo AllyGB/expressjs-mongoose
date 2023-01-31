04035788f5a4f8ac9aa51da8c987e3ade59e3520
AllyGB <94334512+AllyGB@users.noreply.github.com>
2023-01-30 22:22:01 -0600

    Update nodemailer.ts

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -2,10 +2,6 @@ import {createTransport} from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import Mail from "nodemailer/lib/mailer";
 
-if (!process.env.MONGO_URL) {
-  throw new Error("Please add the MONGO_URL environment variable");
-}
-
 function BuildTransport(user: string, pass: string): Mail<SMTPTransport.SentMessageInfo> {
     const transporter = createTransport({
         service: 'gmail',
@@ -21,12 +17,10 @@ function BuildTransport(user: string, pass: string): Mail<SMTPTransport.SentMess
 }
 
 export async function SendMail(message:string, subject: string){
-    if(!process.env.CORREO || !process.env.KEY || !process.env.REMITENTE)  throw new Error("Please add the NODEMAILER environment variable");
-
-    const transporter = BuildTransport(process.env.CORREO, process.env.KEY);
+    const transporter = BuildTransport(process.env.CORREO || "", process.env.KEY || "");
     const sentMail = await transporter.sendMail({
         from: "Alex González",
-        to: process.env.REMITENTE,
+        to: process.env.REMITENTE || "",
         subject: subject,
         html: message,
     })
